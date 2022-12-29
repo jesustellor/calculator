0a00faf6c0fa3883d15cf3e9e42aa1231dd5dbc9
jesustellor <jesustellor@gmail.com>
2022-12-28 23:42:29 -0800

    working calculator

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+
 import '../styles/globals.css'
 
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Component />
+  )
 }
 
 export default MyApp
